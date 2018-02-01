8682e962615b57f7eb98c9e6864b3484df2a798b
Nabin Jaiswal <nabinjaiswal@gmail.com>
2018-02-02 02:05:02 +0530

    implemented crawler to extract Quotes, its author and tags

--- quotes/quotes/spiders/quote.py
+++ quotes/quotes/spiders/quote.py
@@ -8,7 +8,14 @@ class QuoteSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        quotes_tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
-        print quotes_tags
-        
-        
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
+
+            print '\n'
+            print text.encode('ascii', 'ignore')
+            print author
+            print tags
+            print '\n'
