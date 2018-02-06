e721871303751c6163312e316933a659636e7dcb
Nabin Jaiswal <nabinjaiswal@gmail.com>
2018-02-06 16:43:35 +0530

    disable crawling robots.txt and extracted the url with word 'science'

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class BooksSpider(scrapy.Spider):
+class BooksSpider(CrawlSpider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com/']
 
-    def parse(self, response):
+    '''
+    LinkExtractor arguments ==> deny_domains - avoid the domains to scrape present in the website
+                            ==> allow - will scrape the page contain the word in the url
+    '''
+
+
+    rules = (Rule(LinkExtractor(deny_domains=('google.com','facebook.com','twitter.com'),allow=('science')), callback='parse_page', follow=False),)
+
+
+    def parse_page(self, response):
         pass
